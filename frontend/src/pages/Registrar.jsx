c3528dd2c7c4a64c27757cbad604face293610d6
DavidPK8 <alejodavicho8@gmail.com>
2025-04-01 12:00:59 -0500

    Envio de email para confirmacion de cuenta

--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -1,6 +1,64 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Alerta from '../components/Alerta';
 
 const Registrar = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repetirPassword, setRepetirPassword] = useState('');
+
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if([nombre, email, password, repetirPassword].includes('')) {
+      setAlerta({msg: 'Existen campos vacios', error: true});
+      return;
+    }
+
+    if(password !== repetirPassword) {
+      setAlerta({msg: 'Los password no son iguales', error: true});
+      return;
+    }
+
+    if(password.length < 6) {
+      setAlerta({msg: 'El password es muy corto', error: true});
+      return;
+    }
+
+    setAlerta({});
+
+    // Crear el usuario en la api
+    try {
+      const url = "http://localhost:4000/api/veterinarios"
+      await axios.post(url, {nombre, email, password});
+
+      // Mostrar alerta de exito
+      setAlerta({
+        msg: 'Usuario Creado Correctamente, revisa tu email',
+        error: false
+      })
+
+      // Limpiar el formulario
+      setNombre('');
+      setEmail('');
+      setPassword('');
+      setRepetirPassword('');
+    } catch(error) {
+      // Mostrar alerta en caso de que el usuario ya existe
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -11,13 +69,23 @@ const Registrar = () => {
       </div>
 
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-        <form>
+
+        {msg && <Alerta 
+          alerta={alerta}
+        />
+        }
+
+        <form
+          onSubmit={handleSubmit}
+        >
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">Nombre</label>
             <input 
               type="text"  
               placeholder="Tu Nombre"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={event => setNombre(event.target.value)}
             />
           </div>
 
@@ -27,6 +95,8 @@ const Registrar = () => {
               type="email"  
               placeholder="Email de Registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
             />
           </div>
 
@@ -36,6 +106,8 @@ const Registrar = () => {
               type="password"  
               placeholder="Tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
             />
           </div>
 
@@ -45,6 +117,8 @@ const Registrar = () => {
               type="password"  
               placeholder="Repite tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={repetirPassword}
+              onChange={event => setRepetirPassword(event.target.value)}
             />
           </div>
 
